d187e975aa32c5d7fa42a9a4e2b030d3ccca3dbc
Nicolas <nico.leger99@gmail.com>
2024-03-26 18:07:32 +0100

    Retour uniquement du prénom pour le fichier send_mail

--- people_celebration.py
+++ people_celebration.py
@@ -3,5 +3,5 @@ import requests
 url = "http://fetedujour.fr/api/v2/Kmp8YooLJomgowgh/text-normal"
 header = {'User-agent':''}
 response = requests.get(url, headers= header)
+prenom = str(response.content).split(' ')[-1].replace('.', '').replace("'",'')
 
-print(response.content)
