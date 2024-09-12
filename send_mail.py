04d167d11797e03e1bed65603e027ebfc7c5155e
Nicolas <nico.leger99@gmail.com>
2024-09-12 03:13:33 +0200

    Ajout de mots pour quelqu'un

--- send_mail.py
+++ send_mail.py
@@ -6,11 +6,28 @@ from day_celebration import get_day_celebration
 from dotenv import load_dotenv
 import os
 import json
+import random
 
 load_dotenv() 
 lastname = get_lastname_json()
 tab_celebrations = get_day_celebration()
 str_celebrations = ''
+fichier_mots = "mot_liste.txt"
+fichier_mots_utilises = "mot_utilise.txt"
+
+def charger_liste(fichier):
+    try:
+        with open(fichier, "r") as f:
+            return [mot.strip() for mot in f.readlines()]
+    except FileNotFoundError:
+        return []
+    
+def sauvegarder_liste(fichier, liste):
+    with open(fichier, "w") as f:
+        f.write("\n".join(liste))
+
+mots_disponibles = charger_liste(fichier_mots)
+mots_utilises = charger_liste(fichier_mots_utilises)
 
 if tab_celebrations != None:
     for celebration in tab_celebrations:
@@ -19,13 +36,13 @@ if tab_celebrations != None:
 receiver_value = os.getenv("RECEIVER_LIST")
 list_receiver = json.loads(receiver_value)
 
-def core_email(receiver):
+def core_email(receiver, mot=None):
     email_sender = os.getenv("SENDER")
     email_password = os.getenv("PASSWORD_SCIPT_MAIL")
 
     subject = "News Letter Python script"
-    body = "Aujourd'hui c'est la fête des {} ! \n{}".format(lastname, str_celebrations)
-
+    body = "Aujourd'hui c'est la fête des {} ! \n{}\n".format(lastname, str_celebrations)
+    if mot : body += mot
     em = EmailMessage()
 
     em['From'] = email_sender
@@ -40,5 +57,23 @@ def core_email(receiver):
         smtp.sendmail(email_sender, receiver, em.as_string())
 
 
+
 for receiver in list_receiver:
-    core_email(receiver)
+    if mots_disponibles:
+        if receiver == list_receiver[0]:    
+        # Choisir un mot aléatoire pour le destinataire spécial
+            mot = random.choice(mots_disponibles)
+            mot_a_envoyer = "\nMot du jour pour Margot Ferry : " + mot.encode('latin1').decode('utf-8')
+
+            # Ajouter le mot aux mots utilisés et l'enlever des mots disponibles
+            mots_disponibles.remove(mot)
+            mots_utilises.append(mot)
+
+            # Sauvegarder les nouvelles listes
+            sauvegarder_liste(fichier_mots, mots_disponibles)
+            sauvegarder_liste(fichier_mots_utilises, mots_utilises)
+
+            # Envoyer l'email avec le mot spécial
+            core_email(receiver, mot_a_envoyer)
+    else:
+        core_email(receiver)
